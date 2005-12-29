595fca8ba784261f2ff98a24a3bc2c07861a1618
hagish <hagish@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2005-12-29 13:40:50 +0000

    der optimierer arbeitet gut
    git-svn-id: svn://zwischenwelt.org:3690/zw@22 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

--- script/optimizeterrain.php
+++ script/optimizeterrain.php
@@ -3,29 +3,31 @@
 require_once("../lib.main.php");
 require_once("../lib.map.php");
 
-echo "searching 64 segments ";
+echo "searching 64 segments<br>\n";
 $t = sqlgettable("SELECT count(*) AS c, 64*64 AS `cmax`, t.type, floor(`x`/64) AS segx, floor(`y`/64) AS segy FROM `terrain` t GROUP BY segx, segy,type HAVING c=cmax");
+$size = sizeof($t);$count = 0;
 foreach($t as $o)if($o->c==$o->cmax){
   $x = $o->segx;
   $y = $o->segy;
   $type = $o->type;
   sql("REPLACE `terrainsegment64` SET `x`=($x),`y`=($y),`type`=($type)");
   sql("DELETE FROM `terrain` WHERE floor(`x`/64)=($x) AND floor(`y`/64)=($y) AND `type`=$type");
-  echo ".";
+  ++$count;echo "$count / $size <br>\n";
 }
-echo " done<br>\n";
+echo "done<br>\n";
 
 
-echo "searching 4 segments ";
+echo "searching 4 segments<br>\n";
 $t = sqlgettable("SELECT count(*) AS c, 4*4 AS `cmax`, t.type, floor(`x`/4) AS segx, floor(`y`/4) AS segy FROM `terrain` t GROUP BY segx, segy,type HAVING c=cmax");
+$size = sizeof($t);$count = 0;
 foreach($t as $o)if($o->c==$o->cmax){
   $x = $o->segx;
   $y = $o->segy;
   $type = $o->type;
   sql("REPLACE `terrainsegment4` SET `x`=($x),`y`=($y),`type`=($type)");
   sql("DELETE FROM `terrain` WHERE floor(`x`/4)=($x) AND floor(`y`/4)=($y) AND `type`=$type");
-  echo ".";
+  ++$count;echo "$count / $size <br>\n";
 }
-echo " done<br>\n";
+echo "done<br>\n";
 
 ?>
