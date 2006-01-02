568d37719837cc90bfea6f45f99e36e23a93ce4c
ghoulsblade <ghoulsblade@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2006-01-02 18:17:58 +0000

    rumbastel
    git-svn-id: svn://zwischenwelt.org:3690/zw@59 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

--- mapjs7.php
+++ mapjs7.php
@@ -110,7 +110,7 @@ if($gUser && $gUser->usegfxpath && !empty($gUser->gfxpath)){
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/transitional.dtd">
 <html><head>
 <link rel="stylesheet" type="text/css" href="<?=GetZWStylePath()?>"></link>
-<script src="mapjs7_core.js?v=1" type="text/javascript"></script>
+<script src="mapjs7_core.js?v=2" type="text/javascript"></script>
 <script src="<?="mapjs7_globals.js.php".$styleparam?>" type="text/javascript"></script>
 <SCRIPT LANGUAGE="JavaScript" type="text/javascript"><!--
 gCX = <?=intval($gCX)?>;
