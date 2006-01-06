583264df7d5e71c2169a46608aed7ae59ff0f1e4
ghoulsblade <ghoulsblade@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2006-01-06 10:48:10 +0000

    noch mapfix versuch
    git-svn-id: svn://zwischenwelt.org:3690/zw@103 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

--- mapjs7_core.js
+++ mapjs7_core.js
@@ -928,8 +928,8 @@ function navabs (x,y,cancelmode) {
 		mouselistener = document.getElementById("mouselistener_"+iy+"_"+ix);
 		mouselistener.innerHTML = "";
 	}
-	gLoading = true;
-	gAllLoaded = false;
+	//gLoading = true;
+	//gAllLoaded = false;
 	var mode = cancelmode?kJSMapMode_Normal:gMapMode;
 	//location.href = location.pathname + "?sid="+gSID+"&x="+x+"&big="+gBig+"&y="+y+"&cx="+gCX+"&cy="+gCY+"&mode="+mode+"&scroll="+gScroll+"&army="+gActiveArmyID;
 	location.href = kBASEURL + "/mapjs7.php?sid="+gSID+"&x="+x+"&big="+gBig+"&y="+y+"&cx="+gCX+"&cy="+gCY+"&mode="+mode+"&scroll="+gScroll+"&army="+gActiveArmyID;
