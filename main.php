aef59394bc77a868a9b52f67f87842e62c8dd2cf
ghoulsblade <ghoulsblade@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2006-01-10 19:51:42 +0000

    optische unschoenheit mit vielen dummy frames beseitigt
    git-svn-id: svn://zwischenwelt.org:3690/zw@147 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

--- main.php
+++ main.php
@@ -1,8 +1,9 @@
 <?php
 include("lib.main.php");
 Lock();
-$dummyframesetrows_add = "";
-for($i=0;$i<kDummyFrames;++$i) $dummyframesetrows_add .= ",0";
+$dummyframesetcols = array();
+for($i=0;$i<kDummyFrames;++$i) $dummyframesetcols[] = "0";
+$dummyframesetcols = implode(",",$dummyframesetcols);
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
 <html>
@@ -12,19 +13,23 @@ for($i=0;$i<kDummyFrames;++$i) $dummyframesetrows_add .= ",0";
 </head>
 <frameset cols="*,360" noresize>
 	<?php if((intval($gUser->flags) & kUserFlags_ShowLogFrame) == 0){ ?>
-	<frameset rows="*,0<?=$dummyframesetrows_add?>" noresize>
+	<frameset rows="*,0" noresize>
 		<frame src="<?=SessionLink("info.php".((isset($f_fc) && $f_fc==1)?"?fc=1":""));?>" name="info" frameborder="0" noresize>
-		<?php for($i=0;$i<kDummyFrames;++$i) {?>
-		<frame src="about:blank" name="dummy<?=$i?>" noresize frameborder="0">
-		<?php } // endforeach?>
+		<frameset cols="<?=$dummyframesetcols?>" noresize>
+			<?php for($i=0;$i<kDummyFrames;++$i) {?>
+			<frame src="about:blank" name="dummy<?=$i?>" noresize frameborder="0">
+			<?php } // endforeach?>
+		</frameset>
 	</frameset>
 	<?php } else { ?>
-	<frameset rows="*,100<?=$dummyframesetrows_add?>" noresize>
+	<frameset rows="*,100,0" noresize>
 		<frame src="<?=SessionLink("info.php".((isset($f_fc) && $f_fc==1)?"?fc=1":""));?>" name="info" frameborder="1">
 		<frame src="<?=SessionLink("log.php");?>" name="log" frameborder="0">
-		<?php for($i=0;$i<kDummyFrames;++$i) {?>
-		<frame src="about:blank" name="dummy<?=$i?>" noresize frameborder="0">
-		<?php } // endforeach?>
+		<frameset cols="<?=$dummyframesetcols?>" noresize>
+			<?php for($i=0;$i<kDummyFrames;++$i) {?>
+			<frame src="about:blank" name="dummy<?=$i?>" noresize frameborder="0">
+			<?php } // endforeach?>
+		</frameset>
 	</frameset>
 	<?php } ?>
 	<?php if (kMapScript == "mapjs5.php") {?>
