2ee59cd2a58e753f54ad47e5b11a69a2ec2a4d3b
ghoulsblade <ghoulsblade@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2006-03-18 15:46:15 +0000

    typo
    git-svn-id: svn://zwischenwelt.org:3690/zw@295 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

--- lib.fight.php
+++ lib.fight.php
@@ -176,7 +176,7 @@ class cFight {
 				$attack = false;
 				if ($fof == kFOF_Enemy && $autosiege && $canshoot_buildings && $ctype == kUnitContainer_Building) $attack = true;
 				if ($fof == kFOF_Enemy && $autoshoot_enemy)		$attack = true;
-				if ($fof != kFOF_Enemy && && $autoshoot_stranger)	$attack = true;
+				if ($fof != kFOF_Enemy && $autoshoot_stranger)	$attack = true;
 				// start the attack (add to myshootings to consider it right away for next shot
 				if ($attack) {
 					if ($debug) echo "start shooting at $ctype,".oposinfolink($o)."<br>";
