44a83cf15812abe95e0983b7000074e558bc039f
ghoulsblade <ghoulsblade@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2006-03-23 21:48:20 +0000

    debug rausgebaut
    git-svn-id: svn://zwischenwelt.org:3690/zw@326 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

--- mapnavi7.php
+++ mapnavi7.php
@@ -324,7 +324,7 @@ if ($gUser->admin && isset($f_regentypes)) {
 		// vermutlich weil die funktion ueber das map frame aufgerufen wird -> browserbug ?
 		var url = "<?=BASEURL?>info/info.php?x="+x+"&y="+y+urladd+"&sid=<?=$gSID?>";
 		//alert(url);
-		if (mytool == kMapNaviTool_Look || mytool == kMapNaviTool_QuickMagic || 1) 
+		if (mytool == kMapNaviTool_Look || mytool == kMapNaviTool_QuickMagic) 
 				parent.info.location.href = url;
 		else {
 			<?php for ($i=0;$i<kDummyFrames;++$i) {?>
