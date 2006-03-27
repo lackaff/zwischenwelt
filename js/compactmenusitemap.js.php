d96f16542a49a3fe51e4e6347195cb33beb96e84
hagish <hagish@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2006-03-27 22:35:08 +0000

    *menu content update, quickjump
    git-svn-id: svn://zwischenwelt.org:3690/zw@345 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

--- js/compactmenusitemap.js.php
+++ js/compactmenusitemap.js.php
@@ -44,6 +44,15 @@ compactmenusitemap=new Array
   '- Forschung | <a href="info/info.php?x=<?=$hq->x?>&y=<?=$hq->y?>&selectedtab=2'+phpsid+'" target="info">Forschung</a>',
   '- Produktion | <a href="info/info.php?x=<?=$hq->x?>&y=<?=$hq->y?>&selectedtab=3'+phpsid+'" target="info">Produktion</a>',
   '- Diplomatie | <a href="info/info.php?x=<?=$hq->x?>&y=<?=$hq->y?>&selectedtab=5'+phpsid+'" target="info">Diplomatie</a>',
+  'Sprung',
+  <?php
+    foreach($gBuildingType as $id=>$obj)if($obj->flags & kBuildingTypeFlag_IsInQuickJump){
+      $x = sqlgetobject("SELECT `x`,`y`,`level` FROM `building` WHERE `user`=".$gUser->id." AND `type`=".$obj->id." ORDER BY `level` DESC LIMIT 1");
+      if(!empty($x)){?>
+        '- <?=$obj->name?> | <a target=info href="info/info.php?x=<?=$x->x?>&y=<?=$x->y?>'+phpsid+'" title="<?=$obj->name?> (<?=$x->x?>,<?=$x->y?>)"><img src="<?=GetBuildingPic($obj,false,$x->level)?>" border="0" title="<?=$obj->name?> (<?=$x->x?>,<?=$x->y?>)"></a>',
+      <?php }
+    }
+  ?>
   'Übersicht',
   '- Gebäude | <a href="info/summary_buildings.php?'+phpsid+'" target="info">Gebäude</a>',
   '- Forschung | <a href="info/summary_techs.php?'+phpsid+'" target="info">Forschung</a>',
@@ -66,17 +75,6 @@ compactmenusitemap=new Array
   '- Kosten | <a href="info/kosten.php?'+phpsid+'" target="info">Kosten</a>',
   '- Baupläne | <a href="info/bauplan.php?'+phpsid+'" target="info">Baupläne</a>',
   '- Quests | <a href="info/quest.php?'+phpsid+'" target="info">Quests</a>',
-/*
-  'Sprung',
-  <?php
-    foreach($gBuildingType as $id=>$obj){
-      $x = sqlgetobject("SELECT `x`,`y`,`level` FROM `building` WHERE `user`=".$gUser->id." AND `type`=".$obj->id." ORDER BY `level` DESC LIMIT 1");
-      if(!empty($x)){?>
-        '- <?=$obj->name?> | <a target=info href="info/info.php?x=<?=$x->x?>&y=<?=$x->y?>'+phpsid+'" title="<?=$obj->name?> (<?=$x->x?>,<?=$x->y?>)"><img src="<?=GetBuildingPic($obj,false,$x->level)?>" border="0" title="<?=$obj->name?> (<?=$x->x?>,<?=$x->y?>)"></a>',
-      <?php }
-    }
-  ?>
-*/
   'Einheiten',
   '- neu laden | <a href="?sid=<?=$f_sid?>"><img border=0 src="<?=g("icon/reload.png")?>" alt=reload title=reload></a>',
   <?php
