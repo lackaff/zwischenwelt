8a1648fe2c28778b8c57dd533fe2aec60ced7635
ghoulsblade <ghoulsblade@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2006-04-18 14:56:21 +0000

    antinfo
    git-svn-id: svn://zwischenwelt.org:3690/zw@415 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

--- antinfo.php
+++ antinfo.php
@@ -1,4 +1,13 @@
 <?php
 require_once("lib.main.php");
-echo intval(sqlgetone("SELECT COUNT(*) FROM hellhole WHERE ai_type = 3"));
 ?>
+<xml>
+  <antinfo>
+    <timestamp format="Y-m-d H:i:s">2006-04-18 16:51:12</timestamp>
+    <units type="AmeisenTruppenAnzahl"><?=intval(sqlgetone("SELECT COUNT(*) FROM `unit` WHERE `army` > 0 AND `type` = 55"))?></units>
+    <units type="AmeisenKöniginnenTruppenAnzahl"><?=intval(sqlgetone("SELECT COUNT(*) FROM `unit` WHERE `army` > 0 AND  `type` = 54"))?></units>
+    <units type="AmeisenEinheitenAnzahl"><?=intval(sqlgetone("SELECT SUM(`amount`) FROM `unit` WHERE `type` = 55"))?></units>
+    <units type="AmeisenKöniginnenEinheitenAnzahl"><?=intval(sqlgetone("SELECT SUM(`amount`) FROM `unit` WHERE `type` = 54"))?></units>
+    <units type="AmeisenHügel"><?=intval(sqlgetone("SELECT COUNT(*) FROM hellhole WHERE ai_type = 3"))?></units>
+  </antinfo>
+</xml>
