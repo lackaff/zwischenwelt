b6c9eb639421735ed874a2fe668ba3e0998462fa
hagish <hagish@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2006-05-14 23:32:27 +0000

    update checks
    git-svn-id: svn://zwischenwelt.org:3690/zw@449 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

--- setup.php
+++ setup.php
@@ -8,13 +8,12 @@ CheckRequired("FileExists","Configfile 'defines.mysql.php' exists",array("name"=
 include("defines.mysql.php");
 CheckRequired("FileExists","BASEPATH '".BASEPATH."' exists",array("name"=>BASEPATH));
 CheckRequired("FileExists","with an tmp directory",array("name"=>BASEPATH."/tmp"));
+CheckRequired("Writeable","that is writeable",array("name"=>BASEPATH."/tmp"));
 CheckRequired("EregMatch","BASEPATH with trailing /",array("subject"=>BASEPATH,"pattern"=>'^.+(/|\\\\)$'));
 CheckRequired("EregNotMatch","BASEPATH is not an url",array("subject"=>BASEPATH,"pattern"=>CHECK_PATTERN_HTTP));
 CheckRequired("EregMatch","kGfxServerPath is an url",array("subject"=>kGfxServerPath,"pattern"=>CHECK_PATTERN_HTTP));
 CheckRequired("EregMatch","kStyleServerPath is an url",array("subject"=>kStyleServerPath,"pattern"=>CHECK_PATTERN_HTTP));
 CheckRequired("EregMatch","BASEURL is an url",array("subject"=>BASEURL,"pattern"=>'^http://.+$'));
-CheckRequired("FileExists","with an tmp directory",array("name"=>BASEPATH."/tmp"));
-CheckRequired("Writeable","that is writeable",array("name"=>BASEPATH."/tmp"));
 CheckOptional("FileExists","sql error logfile exists",array("name"=>BASEPATH."/sqlerror.log"));
 CheckOptional("Writeable","and is writeable",array("name"=>BASEPATH."/sqlerror.log"));
 CheckRequired("MysqlAccess","mysql access",array("host"=>MYSQL_HOST,"user"=>MYSQL_USER,"pass"=>MYSQL_PASS));
