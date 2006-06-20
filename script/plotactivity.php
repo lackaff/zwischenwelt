1c71842be55358ef6e4cc7717a2770e30d08e374
hagish <hagish@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2006-06-20 13:44:13 +0000

    activity diagram
    git-svn-id: svn://zwischenwelt.org:3690/zw@503 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

---
+++ script/plotactivity.php
@@ -0,0 +1,75 @@
+<?php
+
+include_once("../lib.main.php");
+if($f_mode == "plot"){
+		if(!file_exists("tmp")){
+				mkdir("tmp");
+				chmod("tmp",0777);
+		}
+		
+		$q = "title=".$_REQUEST["title"]."&ids=".$_REQUEST["ids"];
+		$md5 = md5($q);
+
+		if(file_exists("tmp/activity-$md5.png")){
+			header("Location: tmp/activity-$md5.png");
+			exit();
+		}
+
+		header('Content-type: image/png');
+
+		$tmp = "tmp/activity-".$md5;
+
+		$ids = explode(",",$f_ids);
+		$number = 0;
+		foreach($ids as $id){
+				$dat = "#time number\n";
+				
+				$f = fopen($tmp."-$number.dat","w");
+				fputs($f,$dat);
+
+				$t = sqlgettable("SELECT `time` FROM `calllog` WHERE `user`=".intval($id));
+				foreach($t as $x)fputs($f,$x->time." ".$number."\n");
+
+				fclose($f);
+				++$number;
+		}
+
+		$plot = "
+		set terminal postscript eps noenhanced color solid defaultplex \"Verdana\" 16
+		set output \"| convert - $tmp.png\"
+		set grid
+		set size 1,0.7
+		set xdata time
+		set timefmt \"%s\"\n";
+		
+		$ps = array();
+		$number = 0;
+		foreach($ids as $is){
+				$ps[] = "\"$tmp-$number.dat\" using 1:2 title '$id'";
+				++$number;
+		}
+		$plot .= "plot ".implode(", ",$ps);
+
+		$f = fopen($tmp.".plot","w");
+		fputs($f,$plot);
+		fclose($f);
+		$plot = null;
+
+		//$s = 'echo \'set terminal postscript eps noenhanced color solid defaultplex "Verdana" 36\'"\n"\'set output "| convert - '.$tmp.'"\'"\nplot sin(x)" | gnuplot';
+		$s = "gnuplot < $tmp.plot";
+		exec($s);
+		passthru("cat $tmp.png");
+} else {
+		?>
+		<form method=post action=?>
+				User ID Liste: <input type=text name=ids value="<?=$f_ids?>" size=64>
+				<input type=submit value=anzeigen>
+				<?php if(!empty($f_ids)){ ?>
+				<hr>
+				<img src="?mode=plot&ids=<?=$f_ids?>&title=Activity">
+				<hr>
+				<?php } ?>
+		</form>
+		<?php
+}
+?>
