857a77d8cec28c02c1a78e298c70d07c6c4838d8
ghoulsblade <ghoulsblade@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2006-06-22 22:29:43 +0000

    calltime compare multisuche verbessert
    git-svn-id: svn://zwischenwelt.org:3690/zw@514 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

--- script/calltimes.php
+++ script/calltimes.php
@@ -4,13 +4,69 @@ require_once("../lib.main.php");
 
 $gAllUsers = sqlgettable("SELECT * FROM `user`","id");
 $gAllGuilds = sqlgettable("SELECT * FROM `guild`","id");
-$gCmps = array();
+$gAdminUserIDs = sqlgetonetable("SELECT `id` FROM `user` WHERE `admin` = 1");
+$gAdminIPs = (count($gAdminUserIDs) > 0) ? sqlgetonetable("SELECT `ip` FROM `calllog` WHERE `user` IN (".implode(",",$gAdminUserIDs).") GROUP BY `ip`") : array();
+
+define("kCompareSessionTimeout",20*60); // a call starts a new session if the last call is this much away
+define("kCompareShortMax",5*60); // max time considered to be short for breaks between sessions
+
+// return = array(session1,session2,...) where session = object(start:timestamp,end:timestamp,startid=calllog,endid:calllog,ip)
+// use GetSessions for caching
+function CalcSessions ($uid) {
+	//echo "CalcSession(".$uid.")<br>"; flush();
+	global $gAdminIPs;
+	$res = array();
+	$last = false;
+	$session = false;
+	$r = sql("SELECT * FROM `calllog` WHERE `user` = ".intval($uid)." ORDER BY `time`");
+	while ($o = mysql_fetch_object($r)) {
+		if (in_array($o->ip,$gAdminIPs)) continue; // ignore sessions possibly caused by symbiosis
+		if (!$last) {
+			// first entry in loop, session started
+			$session->startid = $o->id;
+			$session->start = $o->time;
+			$session->ip = $o->ip; 
+		} else {
+			$sessionchange = false;
+			if ($last->ip != $o->ip) $sessionchange = true; // ipchange -> new session
+			if ($last->time < $o->time - kCompareSessionTimeout) $sessionchange = true; // has not been used for a while -> new session
+			
+			if ($sessionchange) {
+				// $last ist last of old session
+				$session->endid = $last->id;
+				$session->end = $last->time;
+				$res[] = copyobj($session);
+				
+				// $o is first of new session
+				$session = false;
+				$session->startid = $o->id;
+				$session->start = $o->time;
+				$session->ip = $o->ip; 
+			}
+		}
+		$last = $o;
+	}
+	mysql_free_result($r);
+	if ($last) $res[] = $last->time; // end last session
+	return $res;
+}
+
+// caching
+$gAllSessions = array();
+function GetSessions ($uid) {
+	$uid = intval($uid);
+	global $gAllSessions;
+	if (!isset($gAllSessions[$uid])) $gAllSessions[$uid] = CalcSessions($uid);
+	return $gAllSessions[$uid];
+}
 
 $gCol1 = "#00ff88";
 $gCol2 = "#0088ff";
 
-function printcall ($o,$uid1,$uid2) {
+$gCmps = array();
+function printcall ($o,$uid1,$uid2,$comment="") {
 	global $gAllUsers,$gCol1,$gCol2;
+	if (!is_object($o)) $o = sqlgetobject("SELECT * FROM `calllog` WHERE `id` = ".intval($o));
 	$col = ($o->user == $uid1) ? $gCol1 : $gCol2;
 	?>
 	<tr>
@@ -18,6 +74,7 @@ function printcall ($o,$uid1,$uid2) {
 	<td><font color="<?=$col?>"><?=$gAllUsers[$o->user]->name?>[<?=$o->user?>]</font></td>
 	<td><?=$o->ip?></td>
 	<td><?=$o->script?></td>
+	<td><?=$comment?></td>
 	</tr>
 	<?php
 }
@@ -28,11 +85,20 @@ function callcmp ($uid1,$uid2,$showdetails=false) {
 	
 	//echo "callcmp($uid1,$uid2)<br>";
 	
-	if ($uid1 == -1) {
-		$list = sqlgettable("SELECT * FROM `user`","id");
-		foreach ($list as $o) callcmp($o->id,$uid2,$showdetails);
+	if ($uid1 == -1 && $uid2 == -1) {
+		$list1 = sqlgettable("SELECT * FROM `user`");
+		$list2 = $gAllUsers;
+		$cur = 1;
+		$max = count($list1);
+		foreach ($list1 as $a) {
+			//echo "cmp (".$cur."/".$max.") $a->id with *<br>"; flush();
+			foreach ($list2 as $b) if ($b->id > $a->id) callcmp($a->id,$b->id,$showdetails);
+			++$cur;
+		}
 		return;
 	}
+	
+	if ($uid1 == -1) return; // use uid2 !
 	
 	if ($uid2 == -1) {
 		$list = $gAllUsers;
@@ -42,11 +108,64 @@ function callcmp ($uid1,$uid2,$showdetails=false) {
 	
 	if ($uid1 == $uid2) return; // useless comparison
 	
+	$sess1 = GetSessions($uid1);
+	$sess2 = GetSessions($uid2);
+	if (count($sess1) == 0 || count($sess2) == 0) return;
+	
 	$c_overlap = 0;
-	$c_longwait = 0;
 	$c_shortwait = 0;
 	$c_sameip = 0;
+	
+	foreach ($sess1 as $a) {
+		// make sure to compare only "near" sessions
+		$timebefore = 0;
+		$timeafter = 0;
+		foreach ($sess2 as $b) {
+			if ($b->end <= $a->start) $timebefore = $b->start;
+			if ($b->start >= $a->end) { $timeafter = $b->start; break; }
+		}
 		
+		foreach ($sess2 as $b) {
+			if ($b->start < $timebefore) continue;
+			if ($b->start > $timeafter) break;
+			$overlap = true;
+			if ($b->start >= $a->end) $overlap = false;
+			if ($a->start >= $b->end) $overlap = false;
+			
+			if ($overlap) ++$c_overlap;
+			if (!$overlap) {
+				$timediff = ($a->end <= $b->start) ? 
+							($b->start - $a->end) :	// a is before b
+							($a->start - $b->end);	// b is before a
+				if ($timediff < kCompareShortMax) { 
+					++$c_shortwait;
+					// print a and b
+					if ($showdetails) {
+						if ($a->end <= $b->start) 
+								{ $one = $a; $two = $b;} // a is before b
+						else	{ $one = $b; $two = $a;} // b is before a
+						?>
+						<table border=1>
+						<tr><td colspan="5"><font color="red"><?=Duration2Text($timediff)?>...(<?=$timediff?>s)</font></td></tr>
+						<?php printcall($one->startid,$uid1,$uid2,"session-start"); ?>
+						<?php printcall($one->endid,$uid1,$uid2,"session-end"); ?>
+						<?php printcall($two->startid,$uid1,$uid2,"session-start"); ?>
+						<?php printcall($two->endid,$uid1,$uid2,"session-end"); ?>
+						</table>
+						<br>
+						<?php 
+					}
+				} else {
+					//longwait, irrelevant
+				}
+			}
+			
+			if ($a->ip == $b->ip) ++$c_sameip;
+		}
+	}
+		
+	/*	
+	old code
 	$uid1_has_calls = sqlgetone("SELECT 1 FROM `calllog` WHERE `user` = $uid1 LIMIT 1");
 	$uid2_has_calls = sqlgetone("SELECT 1 FROM `calllog` WHERE `user` = $uid2 LIMIT 1");	
 	if (!$uid1_has_calls || !$uid2_has_calls) return; // not comparable, no log data for one of them
@@ -139,10 +258,10 @@ function callcmp ($uid1,$uid2,$showdetails=false) {
 		
 		if ($showdetails) echo "</table>";
 	}
+	*/
 	
 	$mycmp = false;
 	$mycmp->c_overlap = $c_overlap;
-	$mycmp->c_longwait = $c_longwait;
 	$mycmp->c_shortwait = $c_shortwait;
 	$mycmp->c_sameip = $c_sameip;
 	$mycmp->uid1 = $uid1;
@@ -153,11 +272,13 @@ function callcmp ($uid1,$uid2,$showdetails=false) {
 	$mycmp->score = $mycmp->c_shortwait; 
 	
 	// wenn beide gleichzeitig unabhaengig voneinander online sind (überlappung=overlap), ist es unwahrscheinlicher, aber hier von hand untersuchen
-	//if ($mycmp->c_overlap > 0)  $mycmp->score *= 0.2; 
-	if ($mycmp->c_overlap > 0)  $mycmp->score = 1; 
+	if ($mycmp->c_overlap > 0)  $mycmp->score *= 0.5; 
+	//if ($mycmp->c_overlap > 0)  $mycmp->score = 1; 
 	
 	// gleiche ip gehabt -> multi oder nur wg/familie?
 	$mycmp->score += $mycmp->c_sameip * 0.2;
+	
+	if ($mycmp->score < 2) return;
 	
 	$gCmps[] = $mycmp;
 }
@@ -191,7 +312,6 @@ if (count($gCmps) > 0) {
 		<th>score</th>
 		<th>short<br>wait</th>
 		<th>over<br>lap</th>
-		<th>long<br>wait</th>
 		<th>same<br>ip</th>
 		<th>uid1</th>
 		<th>uid2</th>
@@ -203,7 +323,6 @@ if (count($gCmps) > 0) {
 		<td><?=$o->score?></td>
 		<td><?=$o->c_shortwait?></td>
 		<td><?=$o->c_overlap?></td>
-		<td><?=$o->c_longwait?></td>
 		<td><?=$o->c_sameip?></td>
 		<td><?=uid2txt($o->uid1)?></td>
 		<td><?=uid2txt($o->uid2)?></td>
@@ -231,11 +350,14 @@ Score ist die multi-wahrscheinlichkeit, hoch = sehr wahrscheinlich multi<br>
 </form>
 
 <form method="post" action="<?=Query("?sid=?")?>">
+	Compare one with all, 
 	uid1=<input type="text" name="uid1" value="1881">
 	<input type="submit" name="cmp_1_all" value="cmp_1_all">
+	 (WARNING ! takes a while)
 </form>
 
 <form method="post" action="<?=Query("?sid=?")?>">
-	Compare all with all (WARNING ! takes a while)
+	Compare all with all
 	<input type="submit" name="cmp_all_all" value="cmp_all_all">
+	(WARNING ! takes a while)
 </form>
