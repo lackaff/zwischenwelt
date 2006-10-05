1f8485e3f5abfe4675653c1a95385bc18e8ff076
ghoulsblade <ghoulsblade@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2006-10-05 16:31:31 +0000

    highscoreinfo erweitert
    git-svn-id: svn://zwischenwelt.org:3690/zw@583 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

--- highscoreinfo.php
+++ highscoreinfo.php
@@ -1,12 +1,13 @@
 <?php
 require_once("lib.main.php");
 
-
+$guilds = sqlgettable("SELECT * FROM `guild` WHERE 1","id");
 $userlist=sqlgettable("SELECT `lastlogin`,`id`,`guild`,`name`,`general_pts`+`army_pts` AS pts FROM `user` WHERE `admin`=0 ORDER BY pts DESC","id");
 $i = 0;
 
 foreach ($userlist as $o) {
-	echo $i.",".$o->name.",".$o->pts."<br>\n";
+	$guild = ($o->guild > 0) ? $guilds[$o->guild] : false;
+	echo $i.",".$o->name.",".$o->pts.",".($guild ? $guild->name : "").",".($guild ? $guild->color : "")."<br>\n";
 	++$i;
 };
 ?>
