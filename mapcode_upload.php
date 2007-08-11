57be5fa985f209ca589c3793c00f54c6231cd0df
ghoulsblade <ghoulsblade@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2007-08-11 15:27:58 +0000

    darian mapcode upload
    git-svn-id: svn://zwischenwelt.org:3690/zw@672 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

--- mapcode_upload.php
+++ mapcode_upload.php
@@ -5,8 +5,12 @@ if (strlen(kMapCodePass) <= 0) exit("specify kMapCodePass in defines.mysql.php")
 
 $gCustomMapCode = sqlgetone("SELECT `code` FROM `mapcode` WHERE `name` = 'Darian'");
 
-if ($f_pass == kMapCodePass && isset($f_save)) {
-	sql("REPLACE INTO `mapcode` SET `name` = 'Darian',`code` = '".addslashes($f_code)."'");
+if (isset($f_save)) {
+	if ($f_pass == kMapCodePass) {
+		sql("REPLACE INTO `mapcode` SET `name` = 'Darian',`code` = '".addslashes($f_code)."'");
+	} else {
+		echo "<h1>WRONG PASSWORD, ACCESS DENIED!</h1>";
+	}
 }
 
 ?>
