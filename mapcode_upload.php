48c27ab0101ac5d94864c9898085d6efdd4057ec
ghoulsblade <ghoulsblade@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2007-08-11 15:54:51 +0000

    darian mapcode upload
    git-svn-id: svn://zwischenwelt.org:3690/zw@681 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

--- mapcode_upload.php
+++ mapcode_upload.php
@@ -4,7 +4,7 @@ require_once("lib.main.php");
 if (strlen(kMapCodePass) <= 0) exit("specify kMapCodePass in defines.mysql.php");
 
 if (isset($f_save)) {
-	if ($f_pass == kMapCodePass) {
+	if ($f_mypass == kMapCodePass) {
 		echo "<h1>code updated</h1>";
 		sql("REPLACE INTO `mapcode` SET `name` = 'Darian',`code` = '".addslashes($f_code)."',`css` = '".addslashes($f_css)."'");
 	} else {
@@ -17,7 +17,7 @@ $gCustomMapCode = sqlgetobject("SELECT * FROM `mapcode` WHERE `name` = 'Darian'"
 ?>
 
 <form method="post" action="?">
-	password : <input type="password" name="pass" value="<?=isset($f_pass)?htmlspecialchars($f_pass):""?>"><br>
+	password : <input type="text" name="mypass" value="<?=isset($f_mypass)?htmlspecialchars($f_mypass):""?>"><br>
 	code : <textarea name="code" cols=60 rows=5><?=htmlspecialchars($gCustomMapCode->code)?></textarea><br>
 	css : <textarea name="css" cols=60 rows=5><?=htmlspecialchars($gCustomMapCode->css)?></textarea><br>
 	<input type="submit" name="save" value="speichern">
