e3583b7c50d746471829e006d57cfb32ff2c0567
ghoulsblade <ghoulsblade@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2007-08-11 15:25:24 +0000

    darian mapcode upload and embedding in map
    git-svn-id: svn://zwischenwelt.org:3690/zw@670 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

---
+++ mapcode_upload.php
@@ -0,0 +1,20 @@
+<?php
+require_once("lib.main.php");
+
+if (strlen(kMapCodePass) <= 0) exit("specify kMapCodePass in defines.mysql.php");
+
+$gCustomMapCode = sqlgetone("SELECT `code` FROM `mapcode` WHERE `name` = 'Darian'");
+
+if ($f_pass == kMapCodePass && isset($f_save)) {
+	sql("REPLACE INTO `mapcode` SET `name` = 'Darian',`code` = '".addslashes($f_code)."'");
+}
+
+?>
+
+<form method="post" action="?">
+	<input type="password" name="pass" value="">
+	<textarea name="code" cols=60 rows=5><?=htmlspecialchars($gCustomMapCode)?></textarea>
+	<input type="submit" name="save" value="speichern">
+</form>
+
+
