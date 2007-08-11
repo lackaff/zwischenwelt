3c165aece3f2c927ccd87c4d57c52f6953a83505
ghoulsblade <ghoulsblade@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2007-08-11 15:37:54 +0000

    darian mapcode upload
    git-svn-id: svn://zwischenwelt.org:3690/zw@673 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

--- mapcode_upload.php
+++ mapcode_upload.php
@@ -3,21 +3,23 @@ require_once("lib.main.php");
 
 if (strlen(kMapCodePass) <= 0) exit("specify kMapCodePass in defines.mysql.php");
 
-$gCustomMapCode = sqlgetone("SELECT `code` FROM `mapcode` WHERE `name` = 'Darian'");
-
 if (isset($f_save)) {
 	if ($f_pass == kMapCodePass) {
-		sql("REPLACE INTO `mapcode` SET `name` = 'Darian',`code` = '".addslashes($f_code)."'");
+		echo "<h1>code updated</h1>";
+		sql("REPLACE INTO `mapcode` SET `name` = 'Darian',`code` = '".addslashes($f_code)."',`css` = '".addslashes($f_css)."'");
 	} else {
 		echo "<h1>WRONG PASSWORD, ACCESS DENIED!</h1>";
 	}
 }
 
+$gCustomMapCode = sqlgetone("SELECT * FROM `mapcode` WHERE `name` = 'Darian'");
+
 ?>
 
 <form method="post" action="?">
 	password : <input type="password" name="pass" value=""><br>
-	code : <textarea name="code" cols=60 rows=5><?=htmlspecialchars($gCustomMapCode)?></textarea><br>
+	code : <textarea name="code" cols=60 rows=5><?=htmlspecialchars($gCustomMapCode->code)?></textarea><br>
+	css : <textarea name="css" cols=60 rows=5><?=htmlspecialchars($gCustomMapCode->css)?></textarea><br>
 	<input type="submit" name="save" value="speichern">
 </form>
 
