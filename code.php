745c4afaf6e9aa702a08a1cad3aebb3644a132b0
hagish <hagish@6ae2b031-6f08-0410-bc4a-c5f2e5668f8d>
2009-01-31 15:34:34 +0000

    unhardcoded changelog in code.php
    git-svn-id: svn://zwischenwelt.org:3690/zw@694 6ae2b031-6f08-0410-bc4a-c5f2e5668f8d

--- code.php
+++ code.php
@@ -9,46 +9,27 @@ include("header.php");
 [ <a href="https://sourceforge.net/cvs/?group_id=138787">CVS</a> 
 (cvs -z3 -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/zwischenwelt co -P zw) eine sehr sehr alte version ]<br>
 
-<p>
-<h1>.: 29.08.06 :.</h1>
-R8 freigegeben: Feuer und noch mehr, nun steht der große rewrite an
-</p>
-<p>
-<h1>.: 15.05.06 :.</h1>
-R7 freigegeben: leichtere Installation, mehr Inhalt, mehr Werkzeuge , Bugfixes und viele GUI Verbesserungen
-</p>
-<p>
-<h1>.: 23.02.06 :.</h1>
-R6 bugfix : die "db.sql" war kaputt, bitte diese hier verwenden : <a href="sql/db.sql">db.sql</a>
-</p>
-<p>
-<h1>.: 21.02.06 :.</h1>
-R6 freigegeben: Fernkampf, Ameisenplage und sehr viele &Uuml;berarbeitungen<br>
-</p>
-<p>
-<h1>.: 28.08.05 :.</h1>
-R4 released mit mehr Zaubern und viel Codeüberarbeitung.
-</p>
-<p>
-<h1>.: 10.07.05 :.</h1>
-R3 released mit Schifffahrt und allerlei Adminzeug.
-</p>
-<p>
-<h1>.: 06.04.05 :.</h1>
-R2 released mit Graphiken :)
-</p>
-<p>
-<h1>.: 24.02.05 :.</h1>
-So Sonntag Abend werden wir den Code zusammenpacken und unter GPL releasen.
-</p>
-<p>
-<h1>.: 21.02.05 :.</h1>
-Wir sind zu dem Entschluss gekommen, dass wir Zwischenwelt unter einer freien
-Lizenz veröffentlichen wollen. Wahrscheinlich wird es die GPL für den Source und
-irgend was anderes für die Graphiken werden. Vielleicht kommen wir diese Woche
-noch dazu mal einen Source Release zusammenzustellen. Wer also interesse daran hat
-einfach im Forum oder hier vorbeischauen.
+<br>
 
-hagish
-</p>
+<span id=changelog><h1>ChangeLog</h1></span>
+<div class="changelog">
+<?php
+	$cl = file("ChangeLog");
+	$l = array();
+	$hd = $cl[0];
+	for($i=1;$i<sizeof($cl);++$i){
+		$line = trim($cl[$i]);
+		if(empty($line)){
+			$hd = trim($cl[$i+1]);
+			++$i;
+		} else $l[$hd] .= $line."<br>";
+	}
+	$i = 0;
+	foreach($l as $d=>$t){++$i; ?>
+	<div class="entry">
+		<span class="date"><?=$d?></span>
+		<span class="text"><?=$t?></span>
+	</div>
+	<?php if($i>5)break;} ?>
+</div>
 <?php include("footer.php"); ?>
